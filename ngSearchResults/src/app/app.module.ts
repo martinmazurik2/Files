a85473e57eddd6affdaa62b0507d52ee4d1e0748
Marty Mazurik <martin.t.mazurik2@aexp.com>
2017-07-24 15:15:37 -0700

    Adding ngSearchResults

---
+++ ngSearchResults/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SearchResultsComponent } from './searchResults/searchResults.component';
+import { MaterialModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
